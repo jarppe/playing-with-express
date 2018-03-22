4d6a5ab600584459fdd8dc64f913268a9b4a82f5
Jarppe <jarppe@metosin.fi>
2018-03-22 21:56:09 +0200

    add inserting messages, better logging, etc

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+const morgan = require("morgan")
+const serveStatic = require("serve-static")
 const app = express()
-
 const mountRoutes = require("./routes")
 
 app.set("port", (process.env.PORT || 5000))
-app.use(express.static("./public"))
-mountRoutes(app)
+app.use(morgan("tiny"))
+app.use(serveStatic("./public"))
+app.use(bodyParser.json())
 
-app.all("/*", (req, resp, next) => {
-  console.log("request:", req.method, req.path, req.query, req.params)
-  next()
-})
+mountRoutes(app)
 
 app.listen(app.get("port"), () => console.log("Node app is running at localhost:" + app.get("port")))
--- src/routes/messages.js
+++ src/routes/messages.js
@@ -2,7 +2,6 @@ const Router = require("express-promise-router")
 const db = require("../db")
 
 const router = new Router()
-
 module.exports = router
 
 router.get("/", async (req, res) => {
@@ -12,5 +11,23 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const { rows } = await db.query("select * from messages where id = $1", [req.params.id])
-  res.send(rows[0])
+  if (rows.length > 0) {
+    res.send(rows[0])
+  } else {
+    res.status(404).send({error: "can't find message with id " + req.params.id})
+  }
+})
+
+router.post("/", async (req, res) => {
+  const { message } = req.body
+  if (message) {
+    db.execute("insert into messages (message) values ($1) returning id", [message], (response) => {
+      res.send({
+        id: response.rows[0].id,
+        message: message
+      })
+    })
+  } else {
+    res.status(400).send({error: "The 'message' is required"})
+  }
 })
